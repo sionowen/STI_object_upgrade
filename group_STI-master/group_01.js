89dde3b0476c64252120d062fe39a95f2e9fdee7
sionowen <sionowen34@gmail.com>
2016-04-28 17:10:49 -0500

    changed array inputs to objects

--- group_STI-master/group_01.js
+++ group_STI-master/group_01.js
@@ -1,17 +1,27 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
+// var atticus = ["Atticus", "2405", "47000", 3];
+// var jem = ["Jem", "62347", "63500", 4];
+// var boo = ["Boo", "11435", "54000", 3];
+// var scout = ["Scout", "6243", "74750", 5];
 
+var Person = function(name, iD, salary, score){
+this.name = name;
+this.iD = iD;
+this.salary = salary;
+this.score = score;
+}
+var atticus = new Person('Atticus', '2405', '47000', 3);
+var jem = new Person('Jem', '62347', '63500', 4);
+var boo = new Person('Boo', '11435', '54000', 3);
+var scout = new Person('Scout', '6243', '74750', 5);
 var employees = [atticus, jem, boo, scout];
 
 function getBonus(employee) {
     var bonusPercent = 0;
     var newArray = [];
 
-    newArray[0] = employee[0];
+    newArray[0] = employee.name;
 
-    switch (employee[3]) {
+    switch (employee.score) {
         case 0:
         case 1:
         case 2:
@@ -28,10 +38,10 @@ function getBonus(employee) {
         default:
             alert("Invalid rating for employee " + employee[0]);
     }
-    if (employee[1].length === 4) {
+    if (employee.iD.length === 4) {
         bonusPercent += 5;
     }
-    if (employee[2] > 65000) {
+    if (employee.salary > 65000) {
         bonusPercent--;
     }
     if (bonusPercent > 13) {
@@ -42,8 +52,8 @@ function getBonus(employee) {
     }
 
     newArray[1] = bonusPercent;
-    newArray[2] = (Math.round(employee[2] * ((bonusPercent / 100) + 1) * 100)) / 100;
-    newArray[3] = Math.round(employee[2] * (bonusPercent / 100));
+    newArray[2] = (Math.round(employee.salary * ((bonusPercent / 100) + 1) * 100)) / 100;
+    newArray[3] = Math.round(employee.salary * (bonusPercent / 100));
     return newArray;
 }
 
